5263a96dc678764062ae7b7e2f2d9e23f87b43ab
Khinshan Khan <khanshan501@gmail.com>
2020-05-10 02:02:08 -0400

    figured out some more process creation details

--- mcm_oss/oss.py
+++ mcm_oss/oss.py
@@ -36,10 +36,10 @@ class OSS:
     def show(self, show_type):
         """TODO: WIP, currently just debug prints"""
         if(show_type == 'r'):
-            print('time', self.memory)
-        elif(show_type == 'i'):
             print(self._rt_ready_queue)
             print(self._common_ready_queue)
+        elif(show_type == 'i'):
+            print(self.memory)
         elif(show_type == 'm'):
             print(self.memory)
 
@@ -50,20 +50,28 @@ class OSS:
         """Determine process information using first fit contiguous memory, if possible."""
         proc_type = "Real-Time" if command == "AR" else "Common"
         # first fit algorithm for memory
-        print(self.memory.keys())
+
+        # get all (if any) contiguous memory blocks with sufficient size
         free_memory = sorted(filter(lambda num: num >= size, self.memory.keys()))
-        if(len(free_memory) < 1):
+        if(len(free_memory) < 1): # early exit if no contiguous memory blocks are available
             return None
-        memory_block = self.memory[free_memory[0]]
+        # pick the first possible 'bucket' per first fit approach
+        free_memory_key = free_memory[0]
+        memory_block = self.memory[free_memory_key]
+        # figure out new process's information
         start = memory_block["start"][0]
         end = start + size - 1
+        # rearrange memory per new usage
         memory_remainder = memory_block["end"][0] - end - 1
-        self.memory[free_memory[0]]["start"].pop()
-        memory_remainder_end = self.memory[free_memory[0]]["end"].pop()
-        if not self.memory[free_memory[0]]["start"]:
-            self.memory.pop(free_memory[0], None)
+        self.memory[free_memory_key]["start"].pop(0)
+        memory_remainder_end = self.memory[free_memory_key]["end"].pop(0)
+        # pop key if no more ranges left for it
+        if not self.memory[free_memory_key]["start"]:
+            self.memory.pop(free_memory_key, None)
+        # in the case the key didn't exist, give it default values
         if memory_remainder not in self.memory and memory_remainder != 0:
             self.memory[memory_remainder] = {"start": [], "end": []}
+        # as long as the range isn't 0, we have usable contiguous memory
         if memory_remainder != 0:
             self.memory[memory_remainder]["start"].append(start + size)
             self.memory[memory_remainder]["end"].append(memory_remainder_end)
